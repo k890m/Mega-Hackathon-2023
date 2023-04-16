b088e020f3eadbfe531e307998b2b594c0be3bf8
k890m <94244306+k890m@users.noreply.github.com>
2023-04-16 08:11:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-import numpy as np
+import streamlit as st
+from PIL import Image
 from keras.preprocessing.image import load_img, img_to_array
+import numpy as np
 from keras.models import load_model
-from flask import Flask, jsonify, request
+import requests
+from bs4 import BeautifulSoup
 
 model = load_model('FV.h5')
 
@@ -13,38 +16,33 @@ labels = {0: 'watermelon',1: 'lettuce',2: 'cabbage',3: 'mango',4: 'paprika',5: '
  'capsicum',34: 'jalepeno',35: 'raddish'
 }
 
-
-def prepare_image(img_path):
-    img = load_img(img_path, target_size=(224, 224, 3))
-    img = img_to_array(img)
-    img = img / 255
-    img = np.expand_dims(img, [0])
-    answer = model.predict(img)
+def processed_img(location):
+    img=load_img(location, target_size=(224, 224, 3))
+    img=img_to_array(img)
+    img=img/225
+    img=np.expand_dims(img,[0])
+    answer=model.predict(img)
     y_class = answer.argmax(axis=-1)
     print(y_class)
     y = " ".join(str(x) for x in y_class)
     y = int(y)
     res = labels[y]
-    print(res)
+    print(res) 
     return res.capitalize()
 
+def run():
+    st.title("Food Identifier")
+    img_file = st.file_uploader("Choose an Image", type=["jpg", "png"])
+    if img_file is not None:
+        img = Image.open(img_file).resize((250, 250))
+        st.image(img, use_column_width=False)
+        save_image_path = './upload_images/' + img_file.name
+        with open(save_image_path, "wb") as f:
+            f.write(img_file.getbuffer())
 
-app = Flask(__name__)
+        if img_file is not None:
+            result = processed_img(save_image_path)
+            print(result)
+            st.success("**Predicted : " + result + '**')
 
-
-@app.route('/predict', methods=['POST'])
-def infer_image():
-    if 'file' not in request.files:
-        return jsonify(error="Please try again. The Image doesn't exist")
-
-    file = request.files.get('file')
-    img_bytes = file.read()
-    img_path = "./upload_images/test.jpg"
-    with open(img_path, "wb") as img:
-        img.write(img_bytes)
-    result = prepare_image(img_path)
-    return jsonify(prediction=result)
-
-
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+run()
